6a1e1401cf82615a632ad637278754f09991cbac
Kenneth Russo <k.russo@f5.com>
2018-11-06 16:59:19 -0700

    Enhanced calls to get only the necessary information for AWS autoscaling
    Also added ability to bypass credentials for local AWS asg (need
    to enable the autoscaleDescribe rule.)

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
 all: autoscale
 	@source setgopath.sh && go build autoscale.go
 
+linux: autoscale
+	@source setgopath.sh && GO_ENABLED=0 GOOS=linux GOARCH=amd64 go build autoscale.go
+
 run: all
 	@source aws-env.sh && ./autoscale
+
+docker:
+	@docker build -t russokj/aws-asg:latest .
+	@docker push russokj/aws-asg
 
 clean:
 	@rm autoscale
--- autoscale.go
+++ autoscale.go
@@ -6,64 +6,96 @@ package main
 
 import (
     "fmt"
+    "log"
+    "os"
 
     "github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/credentials"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/ec2"
     "github.com/aws/aws-sdk-go/service/autoscaling"
+    "github.com/aws/aws-sdk-go/aws/ec2metadata"
 )
 
 func main() {
-    // We can explicitly filter on a specific region or use env vars to do it
-    // sess, err := session.NewSession(&aws.Config{Region: aws.String("us-west-2")})
-
-    // Load session from shared config
-    sess := session.Must(session.NewSessionWithOptions(session.Options{
-        SharedConfigState: session.SharedConfigEnable,
-    }))
-
-    // Create new EC2 client object
-    ec2Svc := ec2.New(sess)
-
-    // Call to get detailed information on each instance
-    result, err := ec2Svc.DescribeInstances(nil)
+    // Create sessiontly filter on a specific region or use env vars to do it
+    sess, err := session.NewSession()
     if err != nil {
-        fmt.Println("Error", err)
+        log.Fatal(err.Error())
+    }
+
+    /*
+     * 1. Create session with the specific AWS account credentials
+     * (defaults to current account if not specified; this requires
+     * the 'autoscaling:Describe*' action to be specified in the IAM role
+     * for the blue master instance)
+     */
+
+    // Required - this must be specified in the CRD
+    autoscaleName := os.Getenv("AWS_AUTOSCALE_GROUP_NAME")
+
+    if autoscaleName == "" {
+        log.Fatal("Must specify the environment variable AWS_AUTOSCALE_GROUP_NAME")
+    }
+
+    // Optional - if specified, all must be specified
+    region := os.Getenv("AWS_REGION")
+    accessKey := os.Getenv("AWS_ACCESS_KEY_ID")
+    secretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+    if region == "" {
+        // Call the metadata server to get our region
+        metaDataSvc := ec2metadata.New(sess)
+        region, err = metaDataSvc.Region()
+        if err == nil {
+            sess, err = session.NewSession(&aws.Config{Region: aws.String(region)})
+        }
     } else {
-        fmt.Println("Success", result)
+        sess, err = session.NewSession(&aws.Config{
+            Region:      aws.String(region),
+            Credentials: credentials.NewStaticCredentials(accessKey, secretKey, "")})
+    }
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+    fmt.Printf("\nAWS region: %s\n", region)
+
+    /*
+     * 2. Get the autoscale group information (list of instances & associated state)
+     */
+    asInput := &autoscaling.DescribeAutoScalingGroupsInput{
+        AutoScalingGroupNames: []*string{aws.String(autoscaleName)},
     }
 
-    fmt.Println("\n\n*****\n\n")
-
-    // Create Autoscale object
-    asSvc := autoscaling.New(sess)
-
-    // Example of finding all instances (we could filter for a specific one)
-    result2, err2 := asSvc.DescribeAutoScalingInstances(nil)
-    if err2 != nil {
-       fmt.Println(err.Error())
-    } else {
-       fmt.Println(result2)
+    asClient := autoscaling.New(sess)
+    asDescription, err := asClient.DescribeAutoScalingGroups(asInput)
+    if err != nil {
+       log.Fatal(err.Error())
     }
 
-    fmt.Println("\n\n*****\n\n")
+    /*
+     * 3. Get the IP (int/ext) and instance name  of all instances in the autoscale group
+     */
+    ec2Client := ec2.New(sess)
+    // (should probably verify we only have 1 group)
+    for _, instance := range asDescription.AutoScalingGroups[0].Instances {
+	fmt.Printf("Instance ID: %s,", *instance.InstanceId)
+	fmt.Printf("  Lifecycle State: %s,", *instance.LifecycleState)
 
-    // Example of specifying a specific autoscale group
-    // (Set input to nil if we want to find all autoscale groups)
-    input := &autoscaling.DescribeAutoScalingGroupsInput{
-        AutoScalingGroupNames: []*string{
-            aws.String("myAutoScaleGroupName"),
-        },
+        // We should add all instance Ids for a single autoscale group to cut down on the api calls
+        ec2Input := &ec2.DescribeInstancesInput{
+            InstanceIds: []*string{aws.String(*instance.InstanceId)},
+        }
+        ecDescription, err := ec2Client.DescribeInstances(ec2Input)
+        if err == nil {
+            fmt.Printf("  Private IP: %s,", *ecDescription.Reservations[0].Instances[0].NetworkInterfaces[0].PrivateIpAddress)
+            fmt.Printf("  Public IP: %s,", *ecDescription.Reservations[0].Instances[0].NetworkInterfaces[0].Association.PublicIp)
+            // FIXME(kenr): This assumes the second index.  We need to traverse looking for a tag where Tags[i].Key == 'Name'
+            fmt.Printf("  Name: %s", *ecDescription.Reservations[0].Instances[0].Tags[2].Value)
+        }
+        fmt.Printf("\n")
     }
-    // for now we just retrieve all
-    input = nil
-
-    result3, err3 := asSvc.DescribeAutoScalingGroups(input)
-    if err3 != nil {
-       fmt.Println(err.Error())
-    } else {
-       // TODO: Cycle through all instances of the autoscale group
-       fmt.Println(result3)
+    if err != nil {
+        log.Fatal(err.Error())
     }
-
 }
